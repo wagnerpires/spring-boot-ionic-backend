e64686c2b043db3cca55760a6f4eea74590eb067
Wagner Pires <wagnerpires@hotmail.com>
2019-06-27 16:09:42 -0300

    Estado e Cidade

--- src/main/java/com/wagnerpires/cursomc/CursomcApplication.java
+++ src/main/java/com/wagnerpires/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.wagnerpires.cursomc.domain.Categoria;
+import com.wagnerpires.cursomc.domain.Cidade;
+import com.wagnerpires.cursomc.domain.Estado;
 import com.wagnerpires.cursomc.domain.Produto;
 import com.wagnerpires.cursomc.repositories.CategoriaRepository;
+import com.wagnerpires.cursomc.repositories.CidadeRepository;
+import com.wagnerpires.cursomc.repositories.EstadoRepository;
 import com.wagnerpires.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -43,6 +53,20 @@ public class CursomcApplication implements CommandLineRunner {
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));		
-		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));	
+		
 	}
 }
---
+++ src/main/java/com/wagnerpires/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.wagnerpires.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wagnerpires.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
