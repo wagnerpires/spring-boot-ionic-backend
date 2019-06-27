df32a01c88833fc64659bd4731e075e37db1094d
Wagner Pires <wagnerpires@hotmail.com>
2019-06-27 15:41:28 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/wagnerpires/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/wagnerpires/cursomc/resources/CategoriaResource.java
@@ -22,17 +22,7 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-		
-//		Categoria cat1 = new Categoria(1, "Informática");
-//		Categoria cat2 = new Categoria(2, "Escritório");
-		
-//		List<Categoria> lista = new ArrayList<>();
-//		lista.add(cat1);
-//		lista.add(cat2);
-		
-//		return lista;
 	}
 }
--- src/main/java/com/wagnerpires/cursomc/services/CategoriaService.java
+++ src/main/java/com/wagnerpires/cursomc/services/CategoriaService.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.wagnerpires.cursomc.domain.Categoria;
 import com.wagnerpires.cursomc.repositories.CategoriaRepository;
+import com.wagnerpires.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
-		//Categoria obj = repo.findOne(id);
-		//return obj;
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		}		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
 }
