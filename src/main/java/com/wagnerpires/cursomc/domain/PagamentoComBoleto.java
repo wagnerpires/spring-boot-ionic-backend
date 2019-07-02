63bdcf3f150a8ad78117f543c709029b0aad5eb0
Wagner Pires <wagnerpires@hotmail.com>
2019-07-02 16:10:23 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/wagnerpires/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/wagnerpires/cursomc/domain/PagamentoComBoleto.java
@@ -4,15 +4,19 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.wagnerpires.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 10L;
 	
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataVencimento;
+	
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataPagamento;
-
+	
 	public PagamentoComBoleto() {
 	}
 	
