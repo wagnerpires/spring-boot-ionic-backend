99ed17c38023babf111c9e76df1733b576a49b47
Wagner Pires <wagnerpires@hotmail.com>
2019-07-07 14:07:01 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/wagnerpires/cursomc/services/CategoriaService.java
+++ src/main/java/com/wagnerpires/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wagnerpires.cursomc.domain.Categoria;
+import com.wagnerpires.cursomc.dto.CategoriaDTO;
 import com.wagnerpires.cursomc.repositories.CategoriaRepository;
 import com.wagnerpires.cursomc.services.exceptions.DataIntegrityException;
 import com.wagnerpires.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
