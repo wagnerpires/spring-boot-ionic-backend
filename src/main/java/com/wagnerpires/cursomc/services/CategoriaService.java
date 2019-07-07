70ca3439c5a5c0822b073663bbc005f9f0feb1c6
Wagner Pires <wagnerpires@hotmail.com>
2019-07-07 15:02:28 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/wagnerpires/cursomc/services/CategoriaService.java
+++ src/main/java/com/wagnerpires/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wagnerpires.cursomc.domain.Categoria;
+import com.wagnerpires.cursomc.domain.Cliente;
 import com.wagnerpires.cursomc.dto.CategoriaDTO;
 import com.wagnerpires.cursomc.repositories.CategoriaRepository;
 import com.wagnerpires.cursomc.services.exceptions.DataIntegrityException;
@@ -34,9 +35,11 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
+
 
 	public void delete(Integer id) {
 		find(id);
@@ -58,5 +61,9 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
