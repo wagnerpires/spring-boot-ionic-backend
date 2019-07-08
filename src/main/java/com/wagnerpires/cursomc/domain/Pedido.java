d97fdc8d350d5b38e286eafe11109841376f6c0b
Wagner Pires <wagnerpires@hotmail.com>
2019-07-08 14:24:21 -0300

    Subtotal e total do Pedido

--- src/main/java/com/wagnerpires/cursomc/domain/Pedido.java
+++ src/main/java/com/wagnerpires/cursomc/domain/Pedido.java
@@ -52,6 +52,15 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public double getValorTotal() {
+		double soma = 0.0;
+	
+		for( ItemPedido ip : itens) {
+			soma += ip.getSubTotal();
+		}
+		return soma;
+	}
 
 	public Integer getId() {
 		return id;
